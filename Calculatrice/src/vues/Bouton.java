240a93fb0f15d0bfc0f55e19ebb8b4376836d935
CamCam56 <cruchita-bambina@hotmail.fr>
2016-06-01 13:13:06 +0200

    pb a partir ligne 13 bouton

--- Calculatrice/src/vues/Bouton.java
+++ Calculatrice/src/vues/Bouton.java
@@ -8,6 +8,29 @@ public class Bouton {
 	
 	public Bouton(){
 		GridLayout gl = new GridLayout(5,4);
+		gl.setHgap(5);
+		gl.setVgap(5);
+		Fenetre.fenetre.setLayout(gl);
+		Fenetre.fenetre.getContentPane().add(new JButton ("C"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("/"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("*"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("Del"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("7"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("8"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("9"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("-"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("4"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("5"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("6"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("+"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("1"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("2"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("3"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("()"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("0"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("."));
+		Fenetre.fenetre.getContentPane().add(new JButton ("+/-"));
+		Fenetre.fenetre.getContentPane().add(new JButton ("="));
 	}
 
 }
--- Calculatrice/src/vues/Fenetre.java
+++ Calculatrice/src/vues/Fenetre.java
@@ -7,23 +7,22 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 
 public class Fenetre {
-	//private JPanel pan = new JPanel ();
-	//private JButton bouton0 = new JButton ("0");
+	private JPanel pan = new JPanel ();
 	
 	public Fenetre(){								//initialisation de la fenetre
 		JFrame fenetre = new JFrame();			//creation fenetre
-		//pan.setBackground(Color.LIGHT_GRAY);	//couleur fond panel
+		pan.setBackground(Color.LIGHT_GRAY);	//couleur fond panel
 		fenetre.setTitle("Caclculatrice");		//ajout d'un titre
 		fenetre.setSize(500, 700);				//taille large haut
 		fenetre.setLocationRelativeTo(null);	//place la fenetre au milieu
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//ferme fenetre avec croix rouge
 		
-		GridLayout gl = new GridLayout(5,4);
-		gl.setHgap(5);
-		gl.setVgap(5);
-		fenetre.setLayout(gl);			//def layout a utiliser sur content pane 5L*4C
+		//GridLayout gl = new GridLayout(5,4);
+		//gl.setHgap(5);
+		//gl.setVgap(5);
+		//fenetre.setLayout(gl);			//def layout a utiliser sur content pane 5L*4C
 		
-		fenetre.getContentPane().add(new JButton ("C"));
+		/*fenetre.getContentPane().add(new JButton ("C"));
 		fenetre.getContentPane().add(new JButton ("/"));
 		fenetre.getContentPane().add(new JButton ("*"));
 		fenetre.getContentPane().add(new JButton ("Del"));
@@ -42,10 +41,10 @@ public class Fenetre {
 		fenetre.getContentPane().add(new JButton ("0"));
 		fenetre.getContentPane().add(new JButton ("."));
 		fenetre.getContentPane().add(new JButton ("+/-"));
-		fenetre.getContentPane().add(new JButton ("="));
+		fenetre.getContentPane().add(new JButton ("="));*/
 		
 		fenetre.setVisible(true);				//affichage fenetre
-		
-		//pan.setVisible(true);					//affichage panel
+		fenetre.setContentPane(pan);
+		pan.setVisible(true);					//affichage panel
 	}
 }
